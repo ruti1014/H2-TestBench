048ee9277b3c8ec2c177ebce37c7e9a4d9d858ab
JonasGeckle <jonas.geckle@web.de>
2023-09-28 11:47:53 +0200

    multiplexer V2

--- Code/Multiplexer/Multiplexer.ino
+++ Code/Multiplexer/Multiplexer.ino
@@ -1,19 +1,44 @@
-#define comPin 25
-#define channelA 16
-#define channelB 17
-#define channelC 18
+/*
+*   Settings:
+*       USB CDC On Boot: "Enabled"
+*       USB Firmware MSC On Boot: "Disabled"
+*       Upload Mode: UART0 / Hardware CDC
+*       USB Mode: "USB-OTG (Tiny-USB)"
+*/
+
+#include <Arduino.h>
+#include <arduino-timer.h>
+
+// ESP32-S3 Pins
+#define comPin 3
+#define channelA 14
+#define channelB 35
+#define channelC 47
+
+#if ARDUINO_USB_CDC_ON_BOOT
+#define HWSerial Serial0
+#define USBSerial Serial
+#else
+#define HWSerial Serial
+USBCDC USBSerial;
+#endif
+
+//------------------------------------------------------------------------------------------------
+// number of buttons
+#define numButtons 3     // max. 8 buttons
+//------------------------------------------------------------------------------------------------
 
 bool readChannel (int channel);
+bool readButtons(void *);
 
-bool CH0 = 0;
-bool lastCH0 = 0;
-bool CH1 = 0;
-bool lastCH1 = 0;
-bool CH2 = 0;
-bool lastCH2 = 0;
+bool CHvalue[8]     = {0,0,0,0,0,0,0,0};
+bool lastCHvalue[8] = {0,0,0,0,0,0,0,0};
+
+auto timer = timer_create_default();
 
 void setup() {
-  Serial.begin(9600);
+  HWSerial.begin(9600);
+  HWSerial.println("setup");
   pinMode(channelA, OUTPUT);
   pinMode(channelB, OUTPUT); 
   pinMode(channelC, OUTPUT); 
@@ -21,45 +46,19 @@ void setup() {
   digitalWrite(channelA, LOW);
   digitalWrite(channelB, HIGH);
   digitalWrite(channelC, LOW);
+
+  timer.every(50, readButtons);
 }
 
 void loop() {
-
-  CH0 = readChannel(0);
-  CH2 = readChannel(2);
-  CH1 = readChannel(1);
-  
-
-  if(lastCH0 != CH0)
-  {
-    Serial.print("Channel 0: ");
-    Serial.println(CH0);
-    lastCH0 = CH0;
-  }
-
-  if(lastCH1 != CH1)
-  {
-    Serial.print("Channel 1: ");
-    Serial.println(CH1);
-    lastCH1 = CH1;
-  }
-
-  if(lastCH2 != CH2)
-  {
-    Serial.print("Channel 2: ");
-    Serial.println(CH2);
-    lastCH2 = CH2;
-  }
-  
-  delay(50);
+  timer.tick();
 }
 
 
 bool readChannel(int channel){
   bool comValue = 0;
 
-  switch(channel)
-  {
+  switch(channel){
     case 0:
       digitalWrite(channelA, LOW);
       digitalWrite(channelB, LOW);
@@ -75,11 +74,56 @@ bool readChannel(int channel){
       digitalWrite(channelB, HIGH);
       digitalWrite(channelC, LOW);
       break;
+    case 3:
+      digitalWrite(channelA, HIGH);
+      digitalWrite(channelB, HIGH);
+      digitalWrite(channelC, LOW);
+      break;
+    case 4:
+      digitalWrite(channelA, LOW);
+      digitalWrite(channelB, LOW);
+      digitalWrite(channelC, HIGH);
+      break;
+    case 5:
+      digitalWrite(channelA, HIGH);
+      digitalWrite(channelB, LOW);
+      digitalWrite(channelC, HIGH);
+      break;
+    case 6:
+      digitalWrite(channelA, LOW);
+      digitalWrite(channelB, HIGH);
+      digitalWrite(channelC, HIGH);
+      break;
+    case 7:
+      digitalWrite(channelA, HIGH);
+      digitalWrite(channelB, HIGH);
+      digitalWrite(channelC, HIGH);
+      break;
     default:
-      Serial.println("Channel not connected !");  
+      HWSerial.println("Wrong channel number !");  
   }
 
+  delay(5);
   comValue = digitalRead(comPin);
-
   return comValue;
+}
+
+bool readButtons(void *){
+  // get current values
+  for(int i=0; i<numButtons; i++){ 
+    CHvalue[i] = readChannel(i);
+  }
+
+  // check if values have changed
+  for(int j=0; j<numButtons; j++){
+    if(lastCHvalue[j] != CHvalue[j]){
+      lastCHvalue[j] = CHvalue[j];
+
+      HWSerial.print("Channel ");
+      HWSerial.print(j);
+      HWSerial.print(": ");
+      HWSerial.println(CHvalue[j]);
+    }
+  }
+  return true;    // to keep timer active
 }
